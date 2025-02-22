4c02cb46786c4778f5c37e137304c7864673adb0
Earnest Achayo <earnytechlive@gmail.com>
2025-02-22 23:40:10 +0300

    added the views for functional and class methods

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book, Library
+from django.views.generic import DetailView
 
 # Create your views here.
+
+# function for listing books
+def list_books(request):
+    books = Book.objects.all()
+    return(request, 'list_books.html', {'books' : books})
+
+
+# class view for displaying library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = "library"
