05b889766d26398f1c19dc51e60604350153573e
Earnest Achayo <earnytechlive@gmail.com>
2025-02-22 13:01:33 +0300

    monir fixes

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -17,18 +17,12 @@ def books_by_author(author_name):
 
 # List all books in a library.
 def books_in_library(library_name):
-    library = Library.objects.get(name=library_name).first()
-    if library:
+        library = Library.objects.get(name=library_name)
         return [book.title for book in library.books.all()]
-    return []
-
-
 
 
 # Retrieve the librarian for a library.
 def librarian_of_library(library_name):
-    library = Library.objects.filter(name=library_name).first()
-    if library:
-        librarian = Librarian.objects.filter(library=library).first()
-        return librarian if librarian else None
-    return None
+        library = Library.objects.get(name=library_name)
+        return library.librarian.name
+  
