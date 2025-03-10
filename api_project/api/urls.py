e86194e88423589905fc64211e83c479208ead86
Earnest Achayo <earnytechlive@gmail.com>
2025-03-10 22:53:47 +0300

    fixes

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookList
+from .views import BookList, BookViewSet
 
+# Define the router and register the viewset
+router = DefaultRouter()
+router.register(r'books', BookViewSet, basename='book')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
-    path('', include(router.urls)),
+    path('', include(router.urls)),  
 ]
