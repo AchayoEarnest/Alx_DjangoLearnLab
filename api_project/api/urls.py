470379d20a92ef392a279017fab527ec8d42f94f
Earnest Achayo <earnytechlive@gmail.com>
2025-03-10 22:47:00 +0300

    added viewsets and configured api/urls to allow default router

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import BookList
 
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
 ]
