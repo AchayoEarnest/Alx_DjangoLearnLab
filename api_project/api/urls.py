b8b79dc2cbd93f57312ec9bd3157245c95ea6510
Earnest Achayo <earnytechlive@gmail.com>
2025-03-10 22:36:52 +0300

    api endpoint created for books

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,5 +3,5 @@ from .views import BookList
 
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+    path('books/', BookList.as_view(), name='book-list'),
 ]
