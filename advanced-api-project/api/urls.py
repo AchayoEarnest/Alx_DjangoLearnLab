8e21392ff96ccf11ba9aa4880675c6114f73aeb3
Earnest Achayo <earnytechlive@gmail.com>
2025-03-16 09:28:07 +0300

    views fix

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookListCreateView, BookDetailView
+from .views import BookListView, BookCreateView, BookDetailView, BookUpdateView, BookDeleteView
 from rest_framework.authtoken.views import obtain_auth_token
 
 
@@ -10,6 +10,10 @@ router = DefaultRouter()
 router.register(r'books', BookViewSet, basename='book')
 
 urlpatterns = [
-    path('books/', BookListCreateView.as_view(), name='book-list'),
+    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
+
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -7,30 +7,31 @@ from .serializers import BookSerializer
 from django.views.generic import ListView, UpdateView, DeleteView
 
 
-class BookListCreateView(generics.ListCreateAPIView):
+class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['title', 'author__name', 'publication_year']
     search_fields = ['title', 'author__name']
     ordering_fields = ['title', 'publication_year']
 
-class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-class BookListView(ListView):
-    model = Book
-    template_name = "mymodel_list.html"
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-
-class BookListUpdateView(UpdateView):
-    model = Book
-    template_name = "mymodel_list.html"
-
-
-class BookListDeleteView(DeleteView):
-    model = Book
-    template_name = "mymodel_list.html"
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
