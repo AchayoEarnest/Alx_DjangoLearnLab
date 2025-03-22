14144e67cecc3fbc24e7d3e1b25ad621451d9f6b
Earnest Achayo <earnytechlive@gmail.com>
2025-03-22 21:54:03 +0300

    fixed url links

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),   
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('posts/<int:post_id>/comments/new/', views.add_comment, name='add_comment'),
-    path('comments/<int:comment_id>/edit/', views.edit_comment, name='edit_comment'),
-    path('comments/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
+    path('comments/<int:pk>/update/', views.edit_comment, name='edit_comment'),
+    path('comments/<int:pk>/delete/', views.delete_comment, name='delete_comment'),
 ]
