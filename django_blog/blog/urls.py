f32a3471ee9f173922ba5075eb21ac1a14f4c72a
Earnest Achayo <earnytechlive@gmail.com>
2025-03-22 13:08:51 +0300

    url link arrangement fix

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('posts/', PostListView.as_view(), name='posts'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new', PostCreateView.as_view(), name='post-create'),  
-    path('posts/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),   
-    path('posts<int:pk>//delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),   
+    path('post<int:pk>//delete/', PostDeleteView.as_view(), name='post-delete'),
     
 ]
