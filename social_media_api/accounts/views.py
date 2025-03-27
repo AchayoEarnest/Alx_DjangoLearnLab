cd05d115f1a7799c4feea4ca7a2791a95e2b4060
Earnest Achayo <earnytechlive@gmail.com>
2025-03-27 10:06:09 +0300

    changed serializer.py to serializers.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,7 +5,7 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
-from .serializer import UserSerializer
+from .serializers import UserSerializer
 
 
 # Create your views here.
